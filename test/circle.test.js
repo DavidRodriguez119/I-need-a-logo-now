e2408b78ca035d94d2874835f61eff2285a8261c
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-10-30 17:37:37 -0400

    circle class & test

---
+++ test/circle.test.js
@@ -0,0 +1,12 @@
+const Circle = require(`../lib/circle`);
+
+const answer = `<svg version="1.1"width="300" height="200"xmlns="http://www.w3.org/2000/svg">
+<circle cx="150" cy="100" r="80" fill="darkblue" />
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">DSR</text>
+</svg>`;
+
+describe(`Circle class`, () => {
+    it(`returns the svg code with the arguments of the new Circle`, () => {
+        expect(new Circle(`DSR`,`white`, `darkblue`).svgCode()).toBe(answer)
+    });
+});
