30c32642b15a74c16ba4c2879d35fdcba4c830a4
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-10-31 01:41:42 -0400

    added the code to create the svg file

--- test/square.test.js
+++ test/square.test.js
@@ -1,8 +1,8 @@
 const Square = require(`../lib/square`);
 
-const answer = `<svg version="1.1"width="300" height="200"xmlns="http://www.w3.org/2000/svg">
-<square cx="150" cy="100" r="80" fill="darkblue" />
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">DSR</text>
+const answer = `<svg version="1.1"width="300" height="300"xmlns="http://www.w3.org/2000/svg">
+<polygon points="30,30 30,270 270,270 270,30 " fill="darkblue"/>
+<text x="150" y="170" font-size="60" text-anchor="middle" fill="white">DSR</text>
 </svg>`;
 
 describe(`Square class`, () => {
--- test/triangle.test.js
+++ test/triangle.test.js
@@ -1,8 +1,8 @@
 const Triangle = require(`../lib/Triangle`);
 
-const answer = `<svg version="1.1"width="300" height="200"xmlns="http://www.w3.org/2000/svg">
-<triangle cx="150" cy="100" r="80" fill="darkblue" />
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">DSR</text>
+const answer = `<svg version="1.1"width="300" height="300"xmlns="http://www.w3.org/2000/svg">
+<polygon points="150,0 0,200 300,200" fill="darkblue"/>
+<text x="150" y="150" font-size="60" text-anchor="middle" fill="white">DSR</text>
 </svg>`;
 
 describe(`Triangle class`, () => {
