79cd28efacf81fb422c61ca4b992329a1b3c5691
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2023-10-31 18:33:46 -0400

    fixed the svg file to be the require size

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,7 +1,7 @@
 const Circle = require(`../lib/circle`);
 
-const answer = `<svg version="1.1"width="300" height="200"xmlns="http://www.w3.org/2000/svg">
-<circle cx="150" cy="100" r="80" fill="darkblue" />
+const answer = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<circle cx="150" cy="100" r="100" fill="darkblue" />
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">DSR</text>
 </svg>`;
 
--- test/triangle.test.js
+++ test/triangle.test.js
@@ -1,6 +1,6 @@
 const Triangle = require(`../lib/Triangle`);
 
-const answer = `<svg version="1.1"width="300" height="300"xmlns="http://www.w3.org/2000/svg">
+const answer = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 <polygon points="150,0 0,200 300,200" fill="darkblue"/>
 <text x="150" y="150" font-size="60" text-anchor="middle" fill="white">DSR</text>
 </svg>`;
